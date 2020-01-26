4966cbed5380f5d3d1431739f953b2d7c1c6c1e8
Ian Forsey <forsey@gmail.com>
2020-01-26 20:40:34 +0000

    Remove dependency on jsonresume theme-manager

--- export.sh
+++ export.sh
@@ -1,45 +1,21 @@
 #!/usr/bin/env bash
 
-# Hacky script to generate CV using local theme and theme manager
-# Requires jsonresume-cli to be installed
-
-THEME=jsonresume-theme-lemonlabs
-
 pushd `dirname $0` > /dev/null
 BASE_DIR=`pwd -P`
 popd > /dev/null
 
-if [ ! -d "target" ]; then
-	mkdir target
+THEME=jsonresume-theme-lemonlabs
+GLOBAL_NODE_MODULES="/usr/local/lib/node_modules"
+
+if [ ! -d "$GLOBAL_NODE_MODULES/$THEME" ]; then
+	ln -s "$BASE_DIR" "$GLOBAL_NODE_MODULES/$THEME"
 fi
 
-cd target
+# Export HTML
+resume export --theme lemonlabs index.html
 
-if [ ! -d "theme-manager" ]; then
-	git clone git@github.com:jsonresume/theme-manager.git
-fi
-
-cd theme-manager
-npm install
-mkdir -p themes
-
-if [ ! -L "themes/$THEME" ]; then
-	ln -s $BASE_DIR/$THEME themes/$THEME
-fi
-
-cd themes/$THEME/1.0.0
-npm install
-
-cd $BASE_DIR/target/theme-manager
-
-node server.js &
-THEME_MANAGER_PID=$!
-echo "Theme manager running as process $THEME_MANAGER_PID"
-
-sleep 5 # Classic. Fixes everything.
-
-cd $BASE_DIR
-export THEME_SERVER=http://localhost:3000/theme/
-resume export --theme=lemonlabs index.html
-
-kill $THEME_MANAGER_PID
+# Export PDF - (npm install -g chrome-headless-render-pdf)
+resume serve &
+RESUME_SERVE_PID=$!
+chrome-headless-render-pdf  --url=http://localhost:4000 --pdf=ian-forsey-resume.pdf
+kill -9 $RESUME_SERVE_PID
